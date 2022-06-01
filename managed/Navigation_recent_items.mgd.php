4bf53ced2dec6d570bd22f7923e0576fdd7af028
Coleman Watts <coleman@civicrm.org>
2022-06-01 14:19:18 -0400

    Fix menu to work with multisite

--- managed/Navigation_recent_items.mgd.php
+++ managed/Navigation_recent_items.mgd.php
@@ -1,9 +1,13 @@
 <?php
 use CRM_Recentmenu_ExtensionUtil as E;
 
-return [
-  [
-    'name' => 'Navigation_recent_items',
+$menuItems = [];
+$domains = \Civi\Api4\Domain::get(FALSE)
+  ->addSelect('id')
+  ->execute();
+foreach ($domains as $domain) {
+  $menuItems[] = [
+    'name' => 'Navigation_recent_items_domain_' . $domain['id'],
     'entity' => 'Navigation',
     'cleanup' => 'always',
     'update' => 'unmodified',
@@ -19,8 +23,9 @@ return [
         'is_active' => TRUE,
         'has_separator' => NULL,
         'parent_id' => NULL,
-        'domain_id' => 'current_domain',
+        'domain_id' => $domain['id'],
       ],
     ],
-  ],
-];
+  ];
+}
+return $menuItems;
